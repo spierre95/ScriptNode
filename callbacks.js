9f730625b8af421542caec04b80649215e7b33e2
spierre95 <stephen.pierre20@gmail.com>
2018-04-16 15:47:49 +0000

    completed forth step, added a callback function

---
+++ callbacks.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  https.get(options,function (response) {
+
+  response.setEncoding('utf8');
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    var htmlChunk = '';
+    htmlChunk += data
+    callback(data);
+  });
+});
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions,printHTML)
